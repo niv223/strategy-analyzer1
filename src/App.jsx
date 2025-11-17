bde68a2cc0e51d0c12ede31733a006f27ade6295
niv223 <wqnynn3@gmail.com>
2025-11-18 00:57:37 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
-import AppLayout from './layout/AppLayout'; export default function App(){return <AppLayout/>;}
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import AppLayout from "./layout/AppLayout";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/app/*" element={<AppLayout />} />
+    </Routes>
+  );
+}
